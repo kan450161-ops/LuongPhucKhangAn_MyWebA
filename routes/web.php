2237e12aa7f6c30c66729b8e520d9a428388c8d4
kan450161-ops <kan450161@gmail.com>
2026-05-12 21:43:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo',[DemoController::class,'index']);
+Route::get('/demo2',[DemoController::class,'index2']);
+Route::get('/demo3',[DemoController::class,'index3']);
+Route::get('/demo4/{id}',[DemoController::class,'index4']);
+Route::get('/demo5/{id?}',[DemoController::class,'index5']);
+Route::get('/demo6/{parram1}/{parram2}',[DemoController::class,'index6']);
