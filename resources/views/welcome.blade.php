fa4347640d3c0a6014169bad38260f0383222a37
kan450161-ops <kan450161@gmail.com>
2026-05-12 19:39:19 +0700

    Init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Tên: Lương Phúc Khang An <br>
+    Tuổi: 20 <br>
+</body>
+</html>
